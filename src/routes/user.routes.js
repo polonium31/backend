5699f952d58e28f3f57e59bda8820f6b142651c5
polonium31 <jenu3181@gmail.com>
2024-03-02 10:58:18 -0500

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   changeCurrentPassword,
   getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
@@ -33,27 +35,16 @@ router.route("/login").post(loginUser);
 //secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/reset-password").post(verifyJWT, changeCurrentPassword);
-router.route("/user-details").post(verifyJWT, getCurrentUser);
-router.route("/update-details").post(verifyJWT, updateAccountDetails);
-router.route("/update-avatar").post(
-  verifyJWT,
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-  ]),
-  updateUserAvatar
-);
-router.route("/update-cover-image").post(
-  verifyJWT,
-  upload.fields([
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  updateUserCoverImage
-);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .post(verifyJWT, upload.single("/avatar"), updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 router.route("/refresh-token").post(refreshAccessToken);
 export default router;
